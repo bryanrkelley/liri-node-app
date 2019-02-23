45008a8a91d5cbd0e77aef04efb0c383bacfda13
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-02-22 20:45:17 -0800

    starting files

---
+++ liri.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+
+var Spotify = require('node-spotify-api');
+
+//console.log(process.env);
+
+var keys = require('./keys.js');
+
+var spotify = new Spotify(keys.spotify);
+
+//-------------------------
+
+
+spotify.search({
+    type: 'track',
+    query: 'All the Small Things'
+}, function (err, data) {
+    if (err) {
+        return console.log('Error occurred: ' + err);
+    }
+
+    console.log(data);
+});
+
+
+
+//-----------
+
+// var command = process.argv[2];
+
+// var media = process.argv[3];
+
+// if (command === 'spotify-this-song') {
+
+// };
