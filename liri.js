7c6b501efb9c7297c82e0f5bb9c6a1d578c0dc54
Efreets <44280987+bryanrkelley@users.noreply.github.com>
2019-02-23 20:52:12 -0800

    created additional keys

--- liri.js
+++ liri.js
@@ -1,12 +1,20 @@
 require('dotenv').config();
 
+//Constructors
 var Spotify = require('node-spotify-api');
 
-//console.log(process.env);
+// var Omdb = require('omdb');
 
+var BandsInTown = require('bandsintown');
+
+//-------------------------
 var keys = require('./keys.js');
 
 var spotify = new Spotify(keys.spotify);
+
+// var omdb = new Omdb(keys.omdb);
+
+var bandsintown = new BandsInTown(keys.bands);
 
 //-------------------------
 
@@ -21,6 +29,54 @@ spotify.search({
     console.log(data);
 });
 
+//---------------------------
+
+// omdb.search('saw', function (err, movies) {
+//     if (err) {
+//         return console.error(err);
+//     }
+
+//     if (movies.length < 1) {
+//         return console.log('No movies were found!');
+//     }
+
+//     movies.forEach(function (data) {
+//         console.log('%s (%d)', data.title, data.year);
+//     });
+
+//     // Saw (2004)
+//     // Saw II (2005)
+// });
+
+// omdb.get({
+//     title: 'Saw',
+//     year: 2004
+// }, true, function (err, data) {
+//     if (err) {
+//         return console.error(err);
+//     }
+
+//     if (!data) {
+//         return console.log('data not found!');
+//     }
+
+//     console.log('%s (%d) %d/10', data.title, data.year, data.imdb.rating);
+//     console.log(data.plot);
+
+//     // Saw (2004) 7.6/10
+//     // Two men wake up at opposite sides of a dirty, disused bathroom, chained
+//     // by their ankles to pipes. Between them lies...
+// });
+
+//---------------------------
+
+
+
+bandsintown
+    .getArtistEventList('Skrillex')
+    .then(function (events) {
+        // return array of events
+    });
 
 
 //-----------
